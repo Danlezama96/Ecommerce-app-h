fdfcb2ec60c1e897d8b3ac6577506a211789632c
Jesus Christian Castillo <christian.castillo.lozano@gmail.com>
2021-07-01 10:39:39 -0500

    Modificacion coneccion base de datos local

--- db.php
+++ db.php
@@ -1,19 +1,16 @@
 <?php
 
-require "config/constants.php";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$servername = HOST;
-$username = USER;
-$password = PASSWORD;
-$db = DATABASE_NAME;
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-// Crear conexion
-$con = mysqli_connect($servername, $username, $password,$db);
-
-// Ver conexion
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
+$con = new mysqli($server, $username, $password, $db);
+if(!isset($con)){
+	echo 'Conexión Fallida en db : ', mysqli_connect_error();
+	
 }
-
 
 ?>
