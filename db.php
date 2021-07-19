4aad22cc3feaca8c63652ee864f43950ff88f055
Jesus Christian Castillo <christian.castillo.lozano@gmail.com>
2021-07-19 17:48:38 -0500

    update

--- db.php
+++ db.php
@@ -1,29 +1,19 @@
-
 <?php
-require('config/constants.php');
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+require "config/constants.php";
 
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
+$servername = HOST;
+$username = USER;
+$password = PASSWORD;
+$db = DATABASE_NAME;
 
-// $con = new mysqli($server, $username, $password, $db);
-// if(!isset($con)){
-// 	echo 'Conexión Fallida en db : ', mysqli_connect_error();
-	
-// }
+// Crear conexion
+$con = mysqli_connect($servername, $username, $password,$db);
 
-	$server = HOST;
-	$username = USER;
-	$password = PASSWORD;
-	$db = DATABASE_NAME;
-
-
-$con= new mysqli($server, $username, $password, $db);
-if(!isset($con)){
-	echo 'Erro en la conexión';
+// Ver conexion
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 
 ?>
