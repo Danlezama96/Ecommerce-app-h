1f3bb92d3e27e76f95ca6e535a0f0a1330615775
Jesus Christian Castillo <christian.castillo.lozano@gmail.com>
2021-07-20 20:40:22 -0500

    actualizar database admin

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,18 +8,8 @@ class Database
 	
 	private $con;
 	public function connect(){
-	
-
-
-		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$this->$con = new mysqli($server, $username, $password, $db);
-return $this->con;
+		$this->con = new Mysqli("localhost", "khristian", "12345678", "ecommerceapp");
+		return $this->con;
 	}
 }
 
