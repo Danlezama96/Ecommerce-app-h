590e678d19de703c09edf7538b3e02cb066a26ae
Jesus Christian Castillo <christian.castillo.lozano@gmail.com>
2021-07-22 20:05:13 -0500

    UPDATE: Database.php DESCR: se modifica la coneccion de local a la nube

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,7 +8,15 @@ class Database
 	
 	private $con;
 	public function connect(){
-		$this->con = new Mysqli("localhost", "khristian", "12345678", "ecommerceapp");
+	
+		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+		$server = $url["host"];
+		$username = $url["user"];
+		$password = $url["pass"];
+		$db = substr($url["path"], 1);
+
+		$this->con = new mysqli($server, $username, $password, $db);
 		return $this->con;
 	}
 }
